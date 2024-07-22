57cf435c13095a44efdab4b9f51b4431f59c9cdb
fabianhanu <fabian.hanu@gmail.com>
2024-07-22 17:19:11 +0300

    car due grid

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Burt Car Management',
-      theme: MaterialTheme(textTheme).lightHighContrast(), // Use the light theme
+      theme: MaterialTheme(textTheme).dark(), // Use the light theme
       home: LoginScreen(),
     );
   }
--- lib/widgets/car_due_dates_tab.dart
+++ lib/widgets/car_due_dates_tab.dart
@@ -10,10 +10,10 @@ class DueDatesTab extends StatelessWidget {
   Widget build(BuildContext context) {
     // Create a list of due dates
     List<Map<String, dynamic>> dueDates = [
-      {'label': 'Next Tax Due', 'date': carData['taxDue'], 'icon': LineIcons.moneyBill, 'step': 1},
-      {'label': 'Next Insurance Due', 'date': carData['insuranceDue'], 'icon': LineIcons.userShield, 'step': 2},
-      {'label': 'Next Service Due', 'date': carData['serviceDue'], 'icon': LineIcons.tools, 'step': 3},
-      {'label': 'Next Inspection Due', 'date': carData['inspectionDue'], 'icon': LineIcons.search, 'step': 4},
+      {'label': 'Next Tax Due', 'date': carData['taxDue'], 'icon': LineIcons.moneyBill},
+      {'label': 'Next Insurance Due', 'date': carData['insuranceDue'], 'icon': LineIcons.userShield},
+      {'label': 'Next Service Due', 'date': carData['serviceDue'], 'icon': LineIcons.tools},
+      {'label': 'Next Inspection Due', 'date': carData['inspectionDue'], 'icon': LineIcons.search},
     ];
 
     // Sort the due dates in descending order
@@ -24,19 +24,27 @@ class DueDatesTab extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Text('Due Dates', style: Theme.of(context).textTheme.titleLarge),
+          SizedBox(height: 16),
           Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(16.0),
             decoration: BoxDecoration(
               color: Theme.of(context).colorScheme.surfaceContainerHighest.withOpacity(0.3),
               borderRadius: BorderRadius.circular(12),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Due Dates', style: Theme.of(context).textTheme.titleLarge),
-                ...dueDates.map((dueDate) => _buildTimelineItem(context, dueDate['label'], dueDate['date'], dueDate['icon'], dueDate['step'])).toList(),
-              ],
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: 2, // Adjust this value to make the grid items taller
+              ),
+              itemCount: dueDates.length,
+              itemBuilder: (context, index) {
+                final dueDate = dueDates[index];
+                return _buildGridItem(context, dueDate['label'], dueDate['date'], dueDate['icon']);
+              },
             ),
           ),
         ],
@@ -44,20 +52,31 @@ class DueDatesTab extends StatelessWidget {
     );
   }
 
-  Widget _buildTimelineItem(BuildContext context, String label, dynamic date, IconData icon, int step) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
+  Widget _buildGridItem(BuildContext context, String label, dynamic date, IconData icon) {
+    bool isFutureDate = false;
+    if (date != null) {
+      final parsedDate = DateTime.tryParse(date);
+      if (parsedDate != null) {
+        isFutureDate = parsedDate.isAfter(DateTime.now());
+      }
+    }
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: isFutureDate ? Theme.of(context).colorScheme.errorContainer.withOpacity(0.5) : Theme.of(context).colorScheme.primary.withOpacity(0.5),
+        borderRadius: BorderRadius.circular(12),
+      ),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Icon(icon, color: Theme.of(context).colorScheme.primary),
+          Icon(icon, color: Theme.of(context).colorScheme.onPrimary),
           SizedBox(width: 8),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Step $step: $label',
+                  label,
                   style: Theme.of(context).textTheme.titleMedium,
                 ),
                 Text(
